d9bfa843561c387c8f51fbf939c86c7c9847aa45
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-03-20 23:45:43 -0600

    Soluciones

--- FirstClass/Program.cs
+++ FirstClass/Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 
 namespace FirstClass
+/* https://platzi.com/blog/namespace-en-c-sharp/
+ * Namespace abarca de forma global
+ * Contiene las clases
+ * 
+ * Los namespaces són útiles para hacernos escribir menos código, ya que cuando declaramos el:
+ *          using system
+ * cuando invoquemos la sentencia de imprimir en consola lo podremos hacer de esta manera:
+ *          Console.WriteLine("Hola Mundo");
+ * En vez de esta:
+ *          System.Console.WriteLine("");
+ * 
+ * 
+ */
 {
     class Program
     {
