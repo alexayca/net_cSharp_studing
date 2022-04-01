37c31543de08f04bf98ecaa2fd97805f230c39f0
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-04-01 11:08:15 -0600

    10 numero y operadores aritmeticos

---
+++ FirstClass/FigureArea.cs
@@ -0,0 +1,20 @@
+﻿// https://docs.microsoft.com/es-es/dotnet/csharp/language-reference/operators/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstClass
+{
+    class FigureArea
+    {
+        static void calculateRectangleArea()
+        {
+            float sideA = 10.1f;
+            float sideB = 20;
+
+            float rectangleArea = sideA * sideB;
+            Console.WriteLine("Tha rectangle area is: " +rectangleArea);
+        }
+    }
+}
