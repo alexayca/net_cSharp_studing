249bc8bcb2bead975d6d9ee48bf751f6b6868373
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-04-01 11:57:26 -0600

    15 Listas

---
+++ FirstClass/ListExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstClass
+{
+    class ListExample
+    {
+        static void Listtacos() 
+        {
+            // Definimos la lista
+            List<string> tacoShoppingList = new List<string>();
+
+            tacoShoppingList.Add("5 Taco de suadero");
+            tacoShoppingList.Add("4 Taco de tripa");
+            tacoShoppingList.Add("8 Taco de pastor");
+            tacoShoppingList.Add("4 Jarritos");
+
+            tacoShoppingList.Remove("5 Taco de suadero");   // La comparacion de string es caseSensitive
+            tacoShoppingList.RemoveAt(1);
+
+            for (int i = 0; i < tacoShoppingList.Count; i++)
+            {
+                Console.WriteLine(tacoShoppingList[i]);
+            }
+
+        }
+    }
+}
