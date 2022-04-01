fc55edc4d7b781ff985d9ab4e011c898706e4f6e
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-04-01 11:28:13 -0600

    12 operadores relacionales

--- FirstClass/LogicalOperators.cs
+++ FirstClass/LogicalOperators.cs
@@ -16,7 +16,24 @@ namespace FirstClass
 
             Console.WriteLine("Tre result of the logical OR is: " + (valueOne || valueTwo));
 
-            Console.WriteLine("Tre result of the logical NOT valueOne is: " + (!valueOne));
+            Console.WriteLine("Tre result of the logical NOTresult is: " + (!result));
         }
+
+
+        static void operadorRelacional() 
+        {
+            int valueOne = 3;
+            int valueTwo = 18;
+            int valueThree = -18;
+
+            Console.WriteLine("The result of (valueOne == valueTwo) is: " + (valueOne == valueTwo));
+            Console.WriteLine("The result of (valueOne != valueTwo) is: " + (valueOne != valueTwo) );
+            Console.WriteLine("The result of (valueOne > valueTwo) is: " + (valueOne > valueTwo) );
+            Console.WriteLine("The result of (valueOne < valueTwo) is: " + (valueOne < valueTwo) );
+            Console.WriteLine("The result of (valueOne >= valueTwo) is: " + (valueOne >= valueTwo) );
+            Console.WriteLine("The result of (valueOne <= valueTwo) is: " + (valueOne <= valueTwo) );
+
+        }
+
     }
 }
