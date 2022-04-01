86e5d749a5b089100907920ae1e4a46568a775d0
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-04-01 12:11:36 -0600

    16 Motodos

---
+++ FirstClass/CapturaDatos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstClass
+{
+    class CapturaDatos
+    {
+        static void muestraPersonales() 
+        {
+            Console.WriteLine("What's your name? Please write your full name ");
+            String fullUserName = Console.ReadLine();
+
+            int height = 175;
+            int age = 22;
+
+            string placeholderText = "The information you requested is: ";
+            string information = placeholderText + "\n User name: " + fullUserName + "\n User age: " + age + "\n User height: " + height;     // Concatenacion
+
+            Console.WriteLine("Hello " + fullUserName + ", Welcome to course");
+            Console.WriteLine(information);
+        }
+
+    }
+}
---
+++ FirstClass/EjemploMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstClass
+{
+    class EjemploMethods
+    {
+        static void muestraEjemplo()
+        {
+            Random rnd = new Random();
+            Console.WriteLine($"Random value betwent 1 - 10: ${rnd.Next(1,10)}");
+        }
+
+
+    }
+}
