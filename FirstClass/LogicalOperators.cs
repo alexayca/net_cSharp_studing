533f050f8f5da2d9b8e5388027cb6332c74a62b8
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-04-01 11:17:57 -0600

    11 operadores logicos

---
+++ FirstClass/LogicalOperators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstClass
+{
+    class LogicalOperators
+    {
+        static void example() 
+        {
+            bool valueOne = true;
+            bool valueTwo = false;
+
+            bool result = valueOne && valueTwo;
+            Console.WriteLine("Tre result of the logical AND is: " + result);
+
+            Console.WriteLine("Tre result of the logical OR is: " + (valueOne || valueTwo));
+
+            Console.WriteLine("Tre result of the logical NOT valueOne is: " + (!valueOne));
+        }
+    }
+}
