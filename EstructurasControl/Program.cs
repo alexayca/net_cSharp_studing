1d525ae09af3f4bd6d9a2a329b02b4f38db08c7a
alexannotations <69085537+alexannotations@users.noreply.github.com>
2022-04-04 13:07:17 -0500

    19 if

---
+++ EstructurasControl/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EstructurasControl
+{
+    class Program
+    {
+
+        static void condicionalIF() {
+            int anyValue = 7;
+            string message = "";
+
+            if (anyValue == 7) {
+                message = "OMG, it's a miracle any Value is 7";
+            }
+            else if (anyValue ==14) {
+                message = "OMG, it's a double miracle any Value is 7";
+            }
+            else {
+                message = "puff, the value wasn't 7";
+            }
+            Console.WriteLine($"{message}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The answer is: ");
+            Program.condicionalIF();
+        }
+    }
+}
