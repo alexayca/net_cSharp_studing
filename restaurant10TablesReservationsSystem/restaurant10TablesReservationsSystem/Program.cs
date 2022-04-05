e4b27eeebff8a6c6bd24aa1cd702ccda1342dbc2
alexayca <90019860+alexayca@users.noreply.github.com>
2022-04-04 21:43:15 -0500

    23 mini Project

---
+++ restaurant10TablesReservationsSystem/restaurant10TablesReservationsSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace restaurant10TablesReservationsSystem
+{
+    class Program {
+        static void Main(string[] args) {
+
+            string[] userNames = new string[10] {"Mike", "", "", "", "", "", "", "", "", ""};
+            int arrayCurrentIndex = 1;
+            bool userType;
+
+            Console.WriteLine("Welcome to the best restaurant in the world!");
+
+            while (true) {
+                Console.WriteLine("Are you a register user? write true, or write false");
+                userType = Convert.ToBoolean(Console.ReadLine());
+
+                if (userType) { 
+                    Console.WriteLine("Hello, you are a registered user, please enter your user name");
+                    string userToSearch = Console.ReadLine();
+                    Console.WriteLine($"The user you searched is {userToSearch}");
+                }
+            }
+        }
+    }
+}
