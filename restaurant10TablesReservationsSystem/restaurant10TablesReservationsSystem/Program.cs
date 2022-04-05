0af45e691accab7bfae830f31de22ca541420802
alexayca <90019860+alexayca@users.noreply.github.com>
2022-04-04 21:59:14 -0500

    24 mini Project

--- restaurant10TablesReservationsSystem/restaurant10TablesReservationsSystem/Program.cs
+++ restaurant10TablesReservationsSystem/restaurant10TablesReservationsSystem/Program.cs
@@ -10,14 +10,34 @@ namespace restaurant10TablesReservationsSystem
 
             Console.WriteLine("Welcome to the best restaurant in the world!");
 
-            while (true) {
-                Console.WriteLine("Are you a register user? write true, or write false");
+            while (arrayCurrentIndex<10) {
+                if (arrayCurrentIndex == 10) {
+                    Console.WriteLine("The restaurant is full, try again next year");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine("\n\nAre you a register user? write true, or write false");
                 userType = Convert.ToBoolean(Console.ReadLine());
 
-                if (userType) { 
+                if (userType) {
                     Console.WriteLine("Hello, you are a registered user, please enter your user name");
                     string userToSearch = Console.ReadLine();
                     Console.WriteLine($"The user you searched is {userToSearch}");
+                    // Si regresa -1 no ha encontrado nada
+                    int index = Array.IndexOf(userNames, userToSearch);
+                    if (index == -1)
+                    {
+                        Console.WriteLine("User not found, try again or register");
+                    }
+                    else {
+                        Console.WriteLine($"Welcome {userNames[index]}, it's a pleasure to give your food");
+                    }
+                }
+                else if (userType ==false) {
+                    Console.WriteLine("Please write and remember your user name.");
+                    userNames[arrayCurrentIndex] = Console.ReadLine();
+                    Console.WriteLine("Your user has been saved successfully\n" +
+                        $"Your User Name is {userNames[arrayCurrentIndex]}");
+                    arrayCurrentIndex++;
                 }
             }
         }
