448140f922cec784cfaf8b7367f395cb54351b6d
alexayca <90019860+alexayca@users.noreply.github.com>
2022-04-04 20:42:20 -0500

    20 switch

--- EstructurasControl/Program.cs
+++ EstructurasControl/Program.cs
@@ -4,10 +4,12 @@ namespace EstructurasControl
 {
     class Program
     {
-
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
         static void condicionalIF() {
             int anyValue = 7;
             string message = "";
+
+            Console.WriteLine("The answer is: ");
 
             if (anyValue == 7) {
                 message = "OMG, it's a miracle any Value is 7";
@@ -21,10 +23,37 @@ namespace EstructurasControl
             Console.WriteLine($"{message}");
         }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+        static void condicionalSwitch() {
+            Console.WriteLine("Enter the selected soda: ");
+            string caseSwitch = Console.ReadLine();
+
+            switch (caseSwitch)
+            {
+                case "cola":
+                    Console.WriteLine("Cola soda - $2 USD");
+                    break;
+                case "lime":
+                    Console.WriteLine("Lime soda - $1 USD");
+                    break;
+                case "orange":
+                    Console.WriteLine("Orange soda - $1.5 USD");
+                    break;
+                case "apple":
+                    Console.WriteLine("Apple soda - $1 USD");
+                    break;
+                default:
+                    Console.WriteLine("ERROR: You did not select a soda or you entered an incorrect value.");
+                    break;
+            }
+        }
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
         static void Main(string[] args)
         {
-            Console.WriteLine("The answer is: ");
-            Program.condicionalIF();
+            
+            //Program.condicionalIF();
+            condicionalSwitch();
         }
     }
 }
